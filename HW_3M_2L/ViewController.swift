2905e60369e0dfe829113f8af4b03d9c73c7262c
Sergejs Tiselskis <slokas85@gmail.con>
2022-08-08 17:57:33 +0300

    Initial Commit

---
+++ HW_3M_2L/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_3M_2L
+//
+//  Created by John Doe on 08/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
